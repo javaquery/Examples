37e92cac61255b4d96ee7295ed078827bae896d3
javaquery <vicky.thakor@javaquery.com>
2016-08-04 09:20:28 +0530

    Fail Fast and Fail Safe Example

---
+++ src/com/javaquery/collections/map/FailFastExample.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javaquery.collections.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author javaQuery
+ * @date 4th August, 2016
+ * Example of Fail Fast using Map.
+ */
+public class FailFastExample {
+    public static void main(String[] args) {
+        Map<String, Double> carPrice = new HashMap<String, Double>();
+        carPrice.put("Porsche", 100000D);
+        carPrice.put("Ferrari", 200000D);
+
+        /* Iterate over Map */
+        for(Map.Entry<String, Double> car : carPrice.entrySet()){
+            System.out.println("Car: " + car.getKey());
+            System.out.println("Price: " + car.getValue());
+            
+            /* Will throw java.util.ConcurrentModificationException */
+            carPrice.put("Lamborghini", 300000D);
+        }
+    }
+}
---
+++ src/com/javaquery/collections/map/FailSafeExample.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javaquery.collections.map;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author javaQuery
+ * @date 4th August, 2016 
+ * Example of Fail Safe using ConcurrentHashMap.
+ */
+public class FailSafeExample {
+
+    public static void main(String[] args) {
+        Map<String, Double> carPrice = new ConcurrentHashMap<String, Double>();
+        carPrice.put("Porsche", 100000D);
+        carPrice.put("Ferrari", 200000D);
+
+        /* Iterate over Map */
+        for (Map.Entry<String, Double> car : carPrice.entrySet()) {
+            System.out.println("Car: " + car.getKey());
+            System.out.println("Price: " + car.getValue());
+
+            /* Will be added in original copy of Map but won't available in Iterator */
+            carPrice.put("Lamborghini", 300000D);
+        }
+        System.out.println("=== loop finished ===");
+        System.out.println("Car: Lamborghini" + ", Price: " + carPrice.get("Lamborghini"));
+    }
+}
