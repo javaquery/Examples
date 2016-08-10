7594c5bfcbc3dff146b4785a7ee6c0559ca2e0a2
javaquery <vicky.thakor@javaquery.com>
2016-08-10 09:24:35 +0530

    Comparator Example

--- src/com/javaquery/bean/Item.java
+++ src/com/javaquery/bean/Item.java
@@ -5,6 +5,8 @@
  */
 package com.javaquery.bean;
 
+import java.util.Comparator;
+
 public class Item {
 
     private String name;
@@ -24,5 +26,36 @@ public class Item {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+    
+    public Comparator<Item> orderByNameASC(){
+        return new Comparator<Item>() {
+            @Override
+            public int compare(Item o1, Item o2) {
+                if(o1 != null && o1.getName() != null
+                        && o2 != null && o2.getName() != null){
+                    return o1.getName().compareTo(o2.getName());
+                }
+                return -1;
+            }
+        };
+    }
+    
+    public Comparator<Item> orderByNameDESC(){
+        return new Comparator<Item>() {
+            @Override
+            public int compare(Item o1, Item o2) {
+                if(o1 != null && o1.getName() != null
+                        && o2 != null && o2.getName() != null){
+                    return o2.getName().compareTo(o1.getName());
+                }
+                return -1;
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "name=" + name + ", price=" + price + '}';
     }
 }
