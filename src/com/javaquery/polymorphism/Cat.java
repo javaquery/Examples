3b5f078b64909547a8bb782de1cfb07defc6df2e
javaquery <vicky.thakor@javaquery.com>
2017-11-07 06:17:30 +0000

    Runtime Polymorphism Example

---
+++ src/com/javaquery/polymorphism/Cat.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javaquery.polymorphism;
+
+/**
+ * @author javaQuery
+ * @date 7th November, 2017
+ * @Github: https://github.com/javaquery/Examples
+ */
+public class Cat extends Animal{
+
+    @Override
+    public void run() {
+        System.out.println("Cat is running...");
+    }
+}
---
+++ src/com/javaquery/polymorphism/RuntimePolymorphismExample.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javaquery.polymorphism;
+
+/**
+ * Example demonstrate Runtime Polymorphism.
+ * 
+ * @author javaQuery
+ * @date 7th November, 2017
+ * @Github: https://github.com/javaquery/Examples
+ */
+public class RuntimePolymorphismExample {
+    public static void main(String[] args) {
+        // super-class
+        Animal animal = new Animal();
+        animal.run();
+        
+        /***********************************************/
+        /* sub-class `Dog` */
+        /***********************************************/
+        Dog dog = new Dog();
+        dog.run();
+        
+        animal = dog;
+        /**
+         * animal.run()
+         * It will call referenced object's `run()` method.
+         * In this case, object of `Dog` class is referred.
+         */
+        animal.run(); // runtime polymorphism
+        
+        
+        /***********************************************/
+        /* sub-class `Cat` */
+        /***********************************************/
+        animal = new Cat();
+        
+         /**
+         * animal.run()
+         * It will call referenced object's `run()` method.
+         * In this case, object of `Cat` class is referred.
+         */
+        animal.run(); // runtime polymorphism
+    }
+}
